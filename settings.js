970984d61e20f2182d8d83c1236ad04cb1d5b3ac
Lycurguz <amp@spear.technology>
2023-01-12 01:12:25 +1100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const HUB_CONTRACT_ADDRESS = "0xe263C67c412A374Ea24eC7075C3DdbdC89b1e381";
-const HUB_API_URL = "http://tartarus.spear.technology/";
-const RPC_URL = "http://tartarus.spear.technology:7545/";
+const HUB_API_URL = "http://localhost/";
+const RPC_URL = "http://ganache.spear.technology:7545/";
 const AUSD_ADDRESS = "0x549BD80b7666e689b8f28FD554a66dC382E2388F";
 
 const nocust = getNocust();
@@ -10,11 +10,8 @@ const gasPriceVal = 20;
 var FROM_PUB = "";
 var TO_PUB = "";
 
-const DEFAULT_PUB = "0x5A62cA211e892C41a91a520821D5020347DCA1a3";
-const DEFAULT_PRIV = "b4d0f75abd00dde4d05520a6c688c53fc02160fd7f8c95df72df08a180b23f63";
-
-//const DEFAULT_PUB = "0xE075CAdAa054aeF4F02C94a91979ED0ECf45b3A8";
-//const DEFAULT_PRIV  = "2dad0c5bf3cd0d5757f9e1d4e4e7c1057c39f8f7675ff9a5c2feceaa351af6b9"
+const DEFAULT_PUB = "0x67F6Dd9380A38c7E9b7973c8A7eCbB8f5BFBF89A";
+const DEFAULT_PRIV = "991ddc4c9466c813583c16b8188aa1ec7fae1d809bed28134dfcb39daef77383";
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
